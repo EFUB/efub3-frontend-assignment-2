5ddd7b94f804fa5f7af747df59e0d4c89cac2107
Jeongmin Cho <jojm0829@gmail.com>
2023-04-15 16:35:53 +0900

    ✨ feat: 투두 수정 기능

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const TodoItem = ({ todoList, setTodoList, id, text, done }) => {
+  const [modifying, setModifying] = useState(false);
   const toggleItem = () => {
     setTodoList(
       todoList.map((item) => (item.id === id ? { ...item, done: !done } : item))
@@ -11,11 +14,33 @@ const TodoItem = ({ todoList, setTodoList, id, text, done }) => {
       })
     );
   };
+  const toggleModify = (e) => {
+    e.preventDefault();
+    setModifying((modifying) => !modifying);
+  };
+  const modifyItem = (e) => {
+    setTodoList(
+      todoList.map((item) =>
+        item.id === id ? { ...item, text: e.target.value } : item
+      )
+    );
+  };
   return (
     <div>
       <button onClick={toggleItem}>done</button>
-      <span style={{ textDecoration: done && "line-through" }}>{text}</span>
-      <button>modify</button>
+      {modifying ? (
+        <form style={{ display: "inline" }} onSubmit={toggleModify}>
+          <input
+            type="text"
+            value={text}
+            onChange={modifyItem}
+            onBlur={toggleModify}
+          />
+        </form>
+      ) : (
+        <span style={{ textDecoration: done && "line-through" }}>{text}</span>
+      )}
+      <button onClick={toggleModify}>modify</button>
       <button onClick={deleteItem}>delete</button>
     </div>
   );
