33fb131f97bc26efdd8a056e77ffe3bcd167b9ba
Jeongmin Cho <jojm0829@gmail.com>
2023-05-03 19:35:48 +0900

    ✨ feat: 날씨 API 가져오기(아직 400 에러 있음)

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const API_KEY = "9865a2f1d2bcb4dd8c68487c70fd2943";
 
 const Weather = () => {
+  const [latitude, setLatitude] = React.useState("");
+  const [longitude, setLongitude] = React.useState("");
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    if (window.navigator.geolocation) {
+      // geolocation 지원할 경우 현재 위치 get
+      window.navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }, []);
+
+  const success = (event) => {
+    setLatitude(event.coords.latitude); // 위도
+    setLongitude(event.coords.longitude); // 경도
+  };
+
+  const error = () => {
+    console.log("fail to get the location");
+  };
+
+  const getWeather = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+
+    const json = await response.json();
+    setWeather(json);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  console.log(latitude, longitude);
+  console.log(weather);
+
   return (
     <div>
       <h1>Weather Page</h1>
